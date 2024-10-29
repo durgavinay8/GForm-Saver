cd93116e89121817209d04996c584820a1c8ea3d
durgavinay8 <durgavinay_balla@srmap.edu.in>
2024-10-29 14:54:43 +0530

    Added Vercel.json file to the backend

--- GFormSaverBackend/index.js
+++ GFormSaverBackend/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-// import serverless from "serverless-http";
 import cookieParser from "cookie-parser";
 import errorHandler from "./middlewares/errorHandler.js";
 import connectDb from "./config/dbConnection.js";
@@ -16,23 +15,18 @@ dotenv.config();
 await connectDb();
 
 const app = express();
-const PORT = process.env.PORT || 6900;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(async (req, res, next) => {
-  console.log("Entered the server");
-  next();
+
+app.use("/", (req, res) => {
+  res.send("It's working! Thank you Vercel <3");
 });
 
-app.use("/api/user", (req, res) => {
-  res.json({
-    message: "hello bro!",
-  });
-});
 app.use("/api/oauth", authRoutes);
 
 app.use(validateAccessToken);
@@ -47,4 +41,3 @@ app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server is listening at PORT: ${PORT}...`);
 });
-// module.exports.handler = serverless(app);
